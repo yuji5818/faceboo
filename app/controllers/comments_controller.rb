64efdd08f678c7a989c9bec10de68bb673ebd7df
yuji05225818@gmail.com <yuji05225818@gmail.com>
2017-03-15 23:48:36 +0900

    コメント機能Ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,8 +9,12 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to topic_path(@topic), notice: 'コメントを投稿しました。' }
+        format.json { render :show, status: :created, location: @comment }
+        # JS形式でレスポンスを返します。
+        format.js { render :index }
       else
         format.html { render :new }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
